ed8235c9eae1743b6a890b598109200ddd9b6d3b
Charlie Wright <charlie@cwgw.co>
2018-04-28 17:56:32 -0700

    Update meta, add google search console verification tag

--- src/components/Meta/Meta.js
+++ src/components/Meta/Meta.js
@@ -33,16 +33,22 @@ function Meta (props) {
       <title>{metaTitle}</title>
       <meta name="description" content="Jim Swaim taught at Devotion School in Brookline and went on to become Vice Principal at Pierce School, Principal of Lawrence School, and Principal of Cabot School in Newton." />
       <meta name="keywords" content="Jim, Swaim, Brookline, School, Committee" />
+      <meta name="og:locale" content="en_US" />
       <meta name="og:type" content="website" />
       <meta name="og:title" content={metaTitle} />
+      <meta name="og:site_name" content={metaTitle} />
       <meta name="og:url" content={metaUrl} />
       <meta name="og:image" content={metaImage} />
+      <meta name="og:image:secure_url" content={metaImage} />
+      <meta name="google-site-verification" content="tkfbrbacW3BAwdDyJ1VTgDH6WVX__NRq_Br2vwXZH-A" />
       <link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,600|Lora:700" rel="stylesheet" />
       <script type="application/ld+json" >{`
         {
           "@context": "http://schema.org",
           "@type": "WebSite",
-          "name": "${siteTitle}"
+          "name": "${siteTitle}",
+          "url": "${metaUrl}",
+          "image": "${metaImage}"
         }
       `}</script>
       <html lang="en" />
